8c4b3756d13b20edc4d4269350b860be6a8ac328
vetri02 <vetri02@gmail.com>
2023-09-25 15:21:12 +0530

    Removed babel, use swc for next compatiblity

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
     setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
     transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+        '^.+\\.(t|j)sx?$': ['@swc/jest'],
     },
   };
   
